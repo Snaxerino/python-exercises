b53f455730fea5da9a5700c689e4c20d096b7734
Snaxerino <113636700+Snaxerino@users.noreply.github.com>
2022-10-21 15:12:34 +0100

    Add files via upload

---
+++ Sequencial Structures/4 bimonthly grades and show average.py
@@ -0,0 +1,8 @@
+# g1, g2... = grade1, grade2...
+g1 = float(input('Grade1: '))
+g2 = float(input('Grade2: '))
+g3 = float(input('Grade3: '))
+g4 = float(input('Grade4: '))
+final_score = (g1 + g2 + g3 + g4)/4
+
+print(f'Putting the 4 notes together the final score is {final_score}')
