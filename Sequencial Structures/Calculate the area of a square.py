4c3c8be68ce0959f028bf2bc737e5f017531bfab
Snaxerino <113636700+Snaxerino@users.noreply.github.com>
2022-10-25 22:20:11 +0100

    Add files via upload

---
+++ Sequencial Structures/Calculate the area of a square.py
@@ -0,0 +1,5 @@
+lenght = float(input('Lenght: '))
+widht = float(input('Widht: '))
+square = lenght * widht
+
+print(f'The size of the square is {square}, being the double of the area {square * 2}.')
