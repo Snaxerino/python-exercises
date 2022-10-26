ec3b8f83ba95c823931e5f293233d34bc365f55a
Snaxerino <113636700+Snaxerino@users.noreply.github.com>
2022-10-26 13:49:32 +0100

    Delete Monthly salary (hourly earnings and working hours).py

--- Monthly salary (hourly earnings and working hours).py
+++
@@ -1,6 +0,0 @@
-hours_working = float(input('How many hours do you work? '))
-money_for_hour = float(input('How much do you earn by hour? '))
-
-salary = hours_working * money_for_hour
-
-print(f'If you work for {hours_working} hour/s and earn {money_for_hour} euros by hour by the end of the month you are going to receive {salary} euros.')
