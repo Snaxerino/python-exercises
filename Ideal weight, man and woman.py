87778eae54f29ceac10afcad60e90c00f1600527
Snaxerino <113636700+Snaxerino@users.noreply.github.com>
2022-11-02 08:51:53 +0000

    Delete Ideal weight, man and woman.py

--- Ideal weight, man and woman.py
+++
@@ -1,6 +0,0 @@
-altura = float(input('Indique a sua altura: '))
-
-peso_ideal_homem = (72.7 * altura) - 58
-peso_ideal_mulher = (62.1 * altura) - 44.7
-print(f'O peso ideal do homem é {peso_ideal_homem} kg.')
-print(f'O peso ideal da mulher é {peso_ideal_mulher} kg.')
